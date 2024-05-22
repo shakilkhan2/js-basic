e9b80ba0760481824a675776c425850525f52290
shakilkhan2 <dev.shakilkhan.com>
2024-05-22 16:27:28 +0600

    string

---
+++ string-problems/case.js
@@ -0,0 +1,8 @@
+let input = 'oFfice';
+let saved = 'office';
+
+if(input === saved){
+    console.log('Welcome');
+}else{
+    console.log("Try Again");
+}
---
+++ string-problems/stringLength.js
@@ -0,0 +1,10 @@
+let address = 'Satpara, Doshpara-3518, Daudkandi, Cumilla';
+
+// length:
+console.log(address.length);
+
+// finOut:
+console.log(address[5]);
+const address1 = 'Doshpara'
+console.log(address1.split(2, 5));
+console.log(address1.toLowerCase());
